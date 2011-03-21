a081c9bd39f024e56407a4beab9c50b29c168391
AlSquire <alsquire@gmail.com>
2011-03-21 22:50:04 +0100

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebLapine::Application.config.secret_token = '77391146e99fc624f0df82aaca11e0b649e2cf7be5c4b319b86a323db0f2552ab391bae586616915d70128336de719f01aea8a25ea22e3aa79b7b6399aaab336'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WebLapine::Application.config.session_store :cookie_store, :key => '_WebLapine_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WebLapine::Application.config.session_store :active_record_store
