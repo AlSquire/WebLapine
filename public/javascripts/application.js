ad9e3f3e6b039035a1da7bc39fdcb407c03b293e
AlSquire <alsquire@gmail.com>
2011-07-31 21:17:48 +0200

    Add a button and javascript behavior to show/hide youtube video detected in a link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).observe('dom:loaded', function() {
+  $$('.youtube_video').invoke('hide');
+  $$('.youtube_button').each(function(el) {
+    el.observe('click', function(event) {
+      $(this).up('.link').down('.youtube_video').toggle();
+      event.stop();
+    });
+  });
+});
