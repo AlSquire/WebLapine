2e049212acb640e35d6c3155ef8b5338002cde90
AlSquire <alsquire@gmail.com>
2011-07-31 21:32:33 +0200

    Hiding video div containers at dom:loaded don't prevent them to load  the youtube players if the page is slow to load, so hid with inline styling (no stylesheet because javascript can't handle it)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).observe('dom:loaded', function() {
-  $$('.youtube_video').invoke('hide');
   $$('.youtube_button').each(function(el) {
     el.observe('click', function(event) {
       $(this).up('.link').down('.youtube_video').toggle();
