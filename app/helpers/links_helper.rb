adc2ae38ec921b5856f0b59e078bb0dc7840c316
AlSquire <alsquire@gmail.com>
2011-07-31 20:15:04 +0200

    Add youtube helpers to detect and embed youtube videos on links#index

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,19 @@
 module LinksHelper
+  # Sample youtube urls:
+  #   http://www.youtube.com/watch?v=ZnehCBoYLbc
+  #   http://www.youtube.com/watch?v=_O7iUiftbKU&feature=relmfu
+
+  def youtube_video(video_id)
+    "<iframe width=\"425\" height=\"349\" src=\"http://www.youtube.com/embed/#{video_id}\" frameborder=\"0\" allowfullscreen></iframe>".html_safe
+  end
+
+  def extract_youtube_id_from_url(url)
+    CGI.parse(URI.parse(url).query)["v"].first
+  end
+
+  def detect_youtube_url(line)
+    regexp = /http:\/\/www.youtube.com\/watch\?[^\s]*/
+    match = line.match(regexp)
+    match[0] if match
+  end
 end
--- spec/helpers/links_helper_spec.rb
+++ spec/helpers/links_helper_spec.rb
@@ -11,4 +11,33 @@ require 'spec_helper'
 #   end
 # end
 describe LinksHelper do
+  describe "youtube_video" do
+    it "return an iframe of the video" do
+      helper.youtube_video('ZnehCBoYLbc').should == '<iframe width="425" height="349" src="http://www.youtube.com/embed/ZnehCBoYLbc" frameborder="0" allowfullscreen></iframe>'
+    end
+  end
+
+  describe "extract_youtube_id_from_url" do
+    it "extract the youtube video id from a youtube video url" do
+      url = "http://www.youtube.com/watch?v=ZnehCBoYLbc"
+      helper.extract_youtube_id_from_url(url).should == 'ZnehCBoYLbc'
+    end
+
+    it "extract the youtube video id from a youtube video url with more params" do
+      url = "http://www.youtube.com/watch?v=_O7iUiftbKU&feature=relmfu"
+      helper.extract_youtube_id_from_url(url).should == '_O7iUiftbKU'
+    end
+  end
+
+  describe "detect_youtube_url" do
+    it "detect and return a youtube video url from a string" do
+      line = "This i a really kewl vid: http://www.youtube.com/watch?v=ZnehCBoYLbc !! lol"
+      helper.detect_youtube_url(line).should == "http://www.youtube.com/watch?v=ZnehCBoYLbc"
+    end
+
+    it "return false when there is no youtube video url" do
+      line = "No video here, but a link http://www.yousuck.com/dontwatch?v=ZnehCBoYLbc !!"
+      helper.detect_youtube_url(line).should be_nil
+    end
+  end
 end
