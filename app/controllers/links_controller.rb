97e8cf95d365018b32ea6139adf54681823d0cfa
AlSquire <alsquire@gmail.com>
2012-02-14 23:05:34 +0100

    Use arel syntax instead of SQL in links#index

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,12 +2,12 @@ class LinksController < ApplicationController
   def index
     @network = params[:network]
     @channel = params[:channel]
-    @links   = Link.order('links.created_at DESC').where(:network => @network).where(:channel => @channel)
+    @links   = Link.order(Link.arel_table[:created_at].desc).where(:network => @network).where(:channel => @channel)
     @links   = @links.search(params[:search]) if params[:search]
     respond_to do |format|
       format.html { @links = @links.page(params[:page]) }
       format.rss do
-        @links = @links.where(["links.created_at > ?", 1.week.ago])
+        @links = @links.where(Link.arel_table[:created_at].gt(1.week.ago))
         render :layout => false
       end
     end
