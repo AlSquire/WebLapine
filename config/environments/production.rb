d63bd75ee21f92ac07beccc9e29e787f7fe74a08
AlSquire <alsquire@gmail.com>
2013-03-20 19:42:59 +0100

    Add default host for action mailer so it can send devise reset password for active admin accounts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ WebLapine::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => ENV['ACTION_MAILER_HOST'] }
+
   # Enable threaded mode
   # config.threadsafe!
 
