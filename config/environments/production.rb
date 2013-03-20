a0bde9f0633bc95caad0b4c58ad7c946f3a3e92e
AlSquire <alsquire@gmail.com>
2013-03-20 20:07:47 +0100

    Add Sendgrid smtp config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,16 @@ WebLapine::Application.configure do
 
   config.action_mailer.default_url_options = { :host => ENV['ACTION_MAILER_HOST'] }
 
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
